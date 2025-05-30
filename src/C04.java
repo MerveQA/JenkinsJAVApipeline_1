bb2e9d5cbae184d67fbfeb1e29fae150d55778e5
M.Merve KOŞAR <merveabd.61@gmail.com>
2025-05-30 09:38:52 +0300

    v0.2

--- src/C04.java
+++ src/C04.java
@@ -1,5 +1,9 @@
 public class C04 {
     public static void main(String[] args) {
         System.out.println("ngrok ile github webhook çalıştırması ");
+        System.out.println("Tekrar bir deneme yapıyorum");
+        int a = 5;
+        int toplam = a+5;
+        System.out.println(a);
     }
 }
